6ba382c089248a5d788e829c531d506688f801bb
Nimsaramahagedara <nimsaramahagedara@gmail.com>
2023-10-13 00:16:20 +0530

    MainBannerUpdate

--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -1,18 +1,24 @@
 import { Typography } from 'antd'
 import React from 'react'
-import mainBannerImg from '../assets/MainBannerImage.png'
-const MainBanner = ({name}) => {
+import mainBannerImg from '../assets/banner-image.png'
+import voucherImg  from '../assets/discount.png' 
+const MainBanner = ({ name }) => {
   return (
-    <div className='bg-gradient-to-r from-cyan-800 to-indigo-400 rounded flex items-center shadow-lg my-2'>
-        <div className='img w-2/4'>
-            <img src={mainBannerImg} className='w-full h-full object-contain' />
+    <div className='rounded flex items-center shadow-lg my-2 main-banner'>
+      <div className='img w-3/4'>
+        <img src={mainBannerImg} className='w-full h-full object-contain' />
+      </div>
+      <div className='w-3/4 p-2'>
+        <Typography.Title level={5} className='text-white' style={{ color: 'white' }}><span className='text-yellow-500'>Hello,</span>{name} </Typography.Title>
+        <p className='text-gray-100 text-xs text-center'>Buy any product from FudgedEmojis Store and get upto</p>
+        <div className='flex justify-center'>
+          <p className=' text-yellow-500 text-lg vouchers-text'>Vouchers</p>
+          <img src={voucherImg} className='w-2/4 h-full object-contain' />
         </div>
-        <div className='w-3/4 p-2'>
-            <Typography.Title level={5} className='text-white' style={{color:'white'}}><span className='text-black'>Hello,</span>{name} </Typography.Title>
-            <p className='text-gray-100 text-xs'>Buy any product from FudgedEmojis Store and get upto 100% discount vouchers!</p>
-            <p className='text-black p-1 bg-yellow-500 text-xs'>Limited time Offer</p>
-            <p className='text-white text-xs'>To buy from FudgedEmojis Store  <a href="https://fudgesemoji.myshopify.com/" className='text-white underline'>Click Here</a></p>
-        </div>
+
+        <p className='text-black p-1 bg-yellow-500 text-xs'>Limited time Offer</p>
+        <p className='text-white text-xs' style={{ width: '120%', margin: '10px auto 10px -20%' }}>So Hurry up, buy from FudgedEmojis Store  <a href="https://fudgesemoji.myshopify.com/" className='text-white underline'>Click Here</a></p>
+      </div>
     </div>
   )
 }
