dba031cc4c6a2d475386c2035197267c1a0ed947
Nimsaramahagedara <nimsaramahagedara@gmail.com>
2023-10-17 00:26:01 +0530

    Admin Layout update

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -21,26 +21,26 @@ const items = [
   {
     key: 'sub1',
     icon: React.createElement(AppstoreOutlined),
-    label: 'New Requests',
-    to: 'admin/new'
+    label: 'Send Emails',
+    to: 'admin/send'
   },
-  {
-    key: 'sub2',
-    icon: React.createElement(CheckSquareOutlined),
-    label: 'Accepted',
-    children: [
-      {
-        key: 11,
-        label: 'Send Emails',
-        to: 'admin/send'
-      },
-      {
-        key: 12,
-        label: 'Waiting For Spin',
-        to: 'admin/waiting'
-      },
-    ]
-  },
+  // {
+  //   key: 'sub2',
+  //   icon: React.createElement(CheckSquareOutlined),
+  //   label: 'Accepted',
+  //   children: [
+  //     {
+  //       key: 11,
+  //       label: 'Send Emails',
+  //       to: 'admin/send'
+  //     },
+  //     {
+  //       key: 12,
+  //       label: 'Waiting For Spin',
+  //       to: 'admin/waiting'
+  //     },
+  //   ]
+  // },
   {
     key: 'sub3',
     icon: React.createElement(BarChartOutlined),
